a784bca7ee6156a2c802702461ecaa679e409bf8
Firstname Surname <derek.adams@nuvi.com>
2016-10-16 16:53:43 -0600

    Passing stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,37 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-# stormlight = BookSeries.create({name: "Stormlight Archive"})
+
+stormlight = BookSeries.create({name: "Stormlight Archive"})
 # malazan = BookSeries.create({name: "Malazan Book of the Fallen"})
-# potter = BookSeries.create({name: "Harry Potter"})
-#
+potter = BookSeries.create({name: "Harry Potter"})
+
 # malazan.books.create([{name: "Gardens of the Moon", order: 0}, {name: "Deadhouse Gates", order: 1}])
-# stormlight.books.create([{name: "Way of Kings", order: 0},{name: "Words of Radiance", order: 1}])
-# potter.books.create([{name: "Harry Potter and the Philosopher's Stone", order:0}, {name: "Harry Potter and the Chamber of Secrets", order:1}])
-#
-# chapter_one = stormlight.chapters.find_or_create_by({id: 0})
+stormlight.books.create([{name: "Way of Kings", order: 0},{name: "Words of Radiance", order: 1}])
+potter.books.create([{name: "Harry Potter and the Philosopher's Stone", order:0}, {name: "Harry Potter and the Chamber of Secrets", order:1}])
 
-Book.first.chapters.create({name: "Gardens of the Moon - Chapter 2"})
 
-# szeth = Character.create({name: "Szeth"}).save
-# kaladin = Character.create({name: 'Kaladin'}).save
-#
-# szeth.character_details.create({
-#     chapter_id: 0,
-#     location: 'Kholinar',
-#     details: 'Szeth assassinates King Gavilar'
-#                                })
-# kaladin.character_details.create([{
-#     chapter_id: 1,
-#     location: 'Killing Fields',
-#     details: 'Kaladin is the squad leader over a contingent that aqcuires Cenn, a new recruit'
-#                                  },
-#                                   {
-#                                       chapter_id: 2,
-#                                       location: 'Caravan Squad',
-#                                       details: 'Kaladin, now a slave is escorted to the shattered plains'
-#                                   }
-#                                  ])
+# Book.first.chapters.create({name: "Gardens of the Moon - Chapter 2"})
+
+szeth = Character.create({name: "Szeth"}).save
+kaladin = Character.create({name: 'Kaladin'}).save
+
+szeth.character_details.create({
+    chapter_id: 0,
+    location: 'Kholinar',
+    details: 'Szeth assassinates King Gavilar'
+                               })
+kaladin.character_details.create([{
+    chapter_id: 1,
+    location: 'Killing Fields',
+    details: 'Kaladin is the squad leader over a contingent that aqcuires Cenn, a new recruit'
+                                 },
+                                  {
+                                      chapter_id: 2,
+                                      location: 'Caravan Squad',
+                                      details: 'Kaladin, now a slave is escorted to the shattered plains'
+                                  }
+                                 ])
 
 
 
