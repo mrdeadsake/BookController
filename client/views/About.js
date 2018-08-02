b14df41806ed970fc43f068936f0afd2d3b694ac
Derek Adams <d.3.1415926@gmail.com>
2018-08-02 07:35:21 -0700

    Minor margin fixes

--- client/views/About.js
+++ client/views/About.js
@@ -5,7 +5,7 @@ export default class About extends React.Component {
   render() {
     return(
       <div className="">
-        <div className="">
+        <div className="row__cell">
           <div>My name is Derek Adams, I'm a software engineer living and working in San Diego, CA currently.</div>
           <div>I'm a full-stack engineer developing web applications that include the following in my tech stack:</div>
           <div>ReactJS</div>
